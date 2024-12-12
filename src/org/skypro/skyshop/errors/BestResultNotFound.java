378b433de16cfd3f88c1a091faa4424f349f0a0f
Username117-java <Tupak008@yandex.ru>
2024-12-12 23:08:37 +0530

    OOP_HW_Exception

---
+++ src/org/skypro/skyshop/errors/BestResultNotFound.java
@@ -0,0 +1,15 @@
+package org.skypro.skyshop.errors;
+
+public class BestResultNotFound extends RuntimeException {
+    private String search;
+
+    public BestResultNotFound(String search) {
+        this.search = search;
+    }
+
+    @Override
+    public String toString() {
+        return "Для поискового запроса '" + search + "' не найдено подходящих результатов";
+    }
+
+}
--- src/org/skypro/skyshop/search/SearchEngine.java
+++ src/org/skypro/skyshop/search/SearchEngine.java
@@ -1,26 +1,50 @@
 package org.skypro.skyshop.search;
 
+import org.skypro.skyshop.errors.BestResultNotFound;
+
 public class SearchEngine {
     private Searchable[] searchableMassive;
 
-    public SearchEngine (int massiveSize) {
+    public SearchEngine(int massiveSize) {
         searchableMassive = new Searchable[massiveSize];
     }
 
-    public Searchable[] search(String searchContent) {
-        Searchable[] searchResult = new Searchable[5];
-        int i = 0;
+    public Searchable search(String searchContent) {
+        Searchable searchResult = null;
+        int maxCount = 0;
         for (Searchable s : searchableMassive) {
-            if (s.searchTerm().contains(searchContent)) {
-                searchResult[i] = s;
-                i++;
-                if (i == 5) {
-                    return searchResult;
-                }
+            int index = 0;
+            int count = 0;
+            int subIndex = s.searchTerm().indexOf(searchContent, index);
+            while (subIndex != -1) {
+                count++;
+                index += searchContent.length();
+                subIndex = s.searchTerm().indexOf(searchContent, index);
             }
+            if (count > maxCount) {
+                searchResult = s;
+            }
+        }
+        if (searchResult == null) {
+            throw new BestResultNotFound(searchContent);
         }
         return searchResult;
     }
+
+//    public Searchable[] search(String searchContent) {
+//        Searchable[] searchResult = new Searchable[5];
+//        int i = 0;
+//        for (Searchable s : searchableMassive) {
+//            if (s.searchTerm().contains(searchContent)) {
+//                searchResult[i] = s;
+//                i++;
+//                if (i == 5) {
+//                    return searchResult;
+//                }
+//            }
+//        }
+//        return searchResult;
+//    }
 
     public void add(Searchable searchable) {
         if (searchableMassive[searchableMassive.length - 1] != null) {
