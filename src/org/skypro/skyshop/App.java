61cb2ef5f27c323ca4b1ca3f71aee5224487dee6
Username117-java <Tupak008@yandex.ru>
2024-12-18 18:00:53 +0530

    HW_Collection-List

--- src/org/skypro/skyshop/App.java
+++ src/org/skypro/skyshop/App.java
@@ -10,6 +10,8 @@ import org.skypro.skyshop.search.SearchEngine;
 import org.skypro.skyshop.search.Searchable;
 
 import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
 
 public class App {
     public static void main(String[] args) {
@@ -19,32 +21,14 @@ public class App {
         Product lime = new FixPriceProduct("Лайм");
         Product woodenNails = new FixPriceProduct("Деревянные гвозди");
         Product blackCaviar = new SimpleProduct("Черная икра", 6500);
-        Searchable nailsArticle = new Article("О пользе деревяных гвоздей",
+        Article nailsArticle = new Article("О пользе деревяных гвоздей",
                 "Основная польза применения деревяных гвоздей в уменьшении массы изготавливаемого изделия. " +
                         "Если изделия окажется недостаточно долговечным, инженеров можно избить палкой...");
-        Searchable stickWorkManual = new Article("Инструкция к палке для битья крапивы",
+        Article stickWorkManual = new Article("Инструкция к палке для битья крапивы",
                 "Ниже будут описаны все варианты применения палки для битья крапивы, а также подробная техника безопасности при эксплуатации: ...");
 
-        try {
-            Product redPants = new DiscountedProduct("Малиновые штаны", 1000, 146);
-        } catch (Exception e) {
-            System.out.println(e.toString());
-        }
 
-        try {
-            Product gravicapa = new SimpleProduct("Гравицапа", -666);
-        } catch (Exception e) {
-            System.out.println(e.toString());
-        }
-
-        try {
-            Product nothing = new FixPriceProduct("   ");
-        } catch (Exception e) {
-            System.out.println(e.toString());
-        }
-
-
-        SearchEngine searchEngine = new SearchEngine(7);
+        SearchEngine searchEngine = new SearchEngine();
         searchEngine.add(tequila);
         searchEngine.add(salt);
         searchEngine.add(stickForHittingNettles);
@@ -56,14 +40,44 @@ public class App {
         try {
             System.out.println(searchEngine.search("палк").toString());
         } catch (Exception e) {
-            System.out.println(e.toString());
+            System.out.printf(e.toString());
         }
+
+        System.out.println();
 
         try {
             System.out.println(searchEngine.search("Что находится за ледяной стеной вокруг плоской земли?").toString());
         } catch (Exception e) {
-            System.out.println(e.toString());
+            System.out.printf(e.toString());
         }
+
+        System.out.println();
+
+        ProductBasket basket1 = new ProductBasket();
+        basket1.addProduct(tequila);
+        basket1.addProduct(woodenNails);
+        basket1.addProduct(stickForHittingNettles);
+        basket1.addProduct(tequila);
+
+        System.out.println();
+        System.out.println("Корзина до удаления продукта: ");
+        basket1.printProductBasket();
+
+
+        System.out.println();
+        System.out.println("Удаленные продукты: " + basket1.deleteProduct("Текила"));
+
+        System.out.println();
+        System.out.println("Корзина после удаления продукта: ");
+        basket1.printProductBasket();
+
+        System.out.println();
+        System.out.println("Удаленные продукты: " + basket1.deleteProduct("Сыр"));
+
+        System.out.println();
+        System.out.println("Корзина после удаления несуществующего продукта: ");
+        basket1.printProductBasket();
+
 
     }
 
